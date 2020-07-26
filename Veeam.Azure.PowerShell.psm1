1bc86d2507fc68d3271466446cc48dc938162e38
tjames192 <tjames192@gmail.com>
2020-07-25 22:10:17 -0700

    Add files via upload

---
+++ Veeam.Azure.PowerShell.psm1
@@ -0,0 +1,8 @@
+# This file only exists to enable making this a "Script" module 
+# instead of a "Manifest" module so that Pester tests 
+# are able to use Pester's Mock and InModuleScope features for internal methods.
+# For more information, refer to:
+# https://github.com/pester/Pester/wiki/Testing-different-module-types
+Set-StrictMode -Version Latest
+$script:BaseUrl = "https://"
+$script:DefaultSession = @{}
